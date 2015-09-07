e56816e1a34e46fdf3a40f731b7aa35e7ec368f3
Andrei Maximov <andrei.maximov@gmail.com>
2015-09-06 19:26:34 -0700

    Add unique MongoDB indexes

--- schema/mongodb-indexes.js
+++ schema/mongodb-indexes.js
@@ -1,1 +1,6 @@
 db.refreshtokens.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
+db.refreshtokens.createIndex({ refreshToken: 1 }, { unique: true });
+
+db.users.createIndex({ username: 1 }, { unique: true });
+
+db.clients.createIndex({ clientId: 1 }, { unique: true });

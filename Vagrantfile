d8193723a8db29a59eccf6696a1f17bcc4977a11
Andrei Maximov <andrei.maximov@gmail.com>
2015-12-31 22:41:54 -0800

    Update dependencies and source for Node.js 5 compatibility

--- Vagrantfile
+++ Vagrantfile
@@ -8,6 +8,12 @@ Vagrant.configure(2) do |config|
 
   config.vm.network "private_network", type: "dhcp"
 
+  # Forward Redis port
+  config.vm.network "forwarded_port", guest: 6379, host: 6379
+
+  # Forward MongoDB port
+  config.vm.network "forwarded_port", guest: 27017, host: 27017
+
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "1024"
   end
--- src/formatters/json.js
+++ src/formatters/json.js
@@ -2,11 +2,11 @@
 
 var restifyJSONFormatter = require('restify/lib/formatters/json');
 
-function formatJSON(req, res, body) {
+function formatJSON(req, res, body, cb) {
   if (body instanceof Error && body.name === 'ValidationError') {
     body = body.details;
   }
-  return restifyJSONFormatter(req, res, body);
+  return restifyJSONFormatter(req, res, body, cb);
 }
 
 module.exports = formatJSON;
